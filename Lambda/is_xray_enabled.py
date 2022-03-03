a0b112d0a07b29fb9e72b27bda0bcd0c952a2245
Scott Gordon <sagordon-dev@outlook.com>
2022-03-03 09:03:52 -0500

    finishing touches

--- Lambda/is_xray_enabled.py
+++ Lambda/is_xray_enabled.py
@@ -15,18 +15,21 @@ for item in response["Functions"]:
 if len(xray_disabled) == 0:
     print("There are no functions currently that have X-Ray disabled.")
 else:
-    print(f"Enabled X-Ray on these Lambda functions: {xray_disabled}")
+    print("These functions have X-Ray disabled.")
     print("Saving functions with X-Ray disabled to 'disabled_xray.txt'")
     with open("disabled_xray.txt", "w") as f:
         for item in xray_disabled:
             f.write(f"{item}\n")
 
 # Enable X-Ray from xray_disabled list.
+print(f"Enabling X-Ray on these Lambda functions: {xray_disabled}")
 for item in xray_disabled:
     response = client.update_function_configuration(
         FunctionName=item, TracingConfig={"Mode": "Active"}
     )
     # Add newly enabled X-Ray items to xray_enabled list.
+    print("Saving functions with X-Ray enabled to 'enabled_xray.txt'")
     xray_enabled.append(item)
-
-print(f"X-Ray is enabled on these Lambda functions: {xray_enabled}")
+    with open("enabled_xray.txt", "w") as f:
+        for item in xray_disabled:
+            f.write(f"{item}\n")
