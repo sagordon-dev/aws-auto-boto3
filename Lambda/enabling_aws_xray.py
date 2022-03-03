bf70d28df1340cad4165ec4b4d19a76cd1173bfe
Scott Gordon <sagordon-dev@outlook.com>
2022-03-03 09:09:03 -0500

    add header

--- Lambda/enabling_aws_xray.py
+++ Lambda/enabling_aws_xray.py
@@ -1,3 +1,10 @@
+'''
+    File name: enabling_aws_xray.py
+    Author: Scott Gordon 
+    Date created: 3/2/2022
+    Date last modified: 3/3/2022
+    Python Version: 3.8 
+'''
 import boto3
 
 xray_disabled = []
